aa53ec6924481c5199c94b919daf1f005ada7e8e
Owocki <ksowocki@gmail.com>
2021-03-01 20:35:11 -0700

    makes gitcoinpassport non-transferable, allows for burning, and makes it so that the end user needs to pay gas fees

--- contracts/GitcoinPassport.sol
+++ contracts/GitcoinPassport.sol
@@ -1,20 +1,54 @@
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "contracts/VerifySignature.sol";
 
-contract GitcoinPassport is ERC721 {
+contract GitcoinPassport is ERC721, VerifySignature {
     address public owner;
 
     constructor() ERC721("GitcoinPassport", "GPP") {
       owner = msg.sender;
     }
 
-  function createPassport(address player, string memory tokenURI) public returns (uint256) {
-      require(msg.sender == owner, "ERC721: must be validated by Gitcoin");
+    /**
+    * @dev Creates a new GitcoinPassport for player at existing address
+    * Passport is non transferable, and cannot be replaced
+    */
+  function createPassport(string memory tokenURI, bytes memory signature, uint _nonce) public returns (uint256) {
+      address player = msg.sender;
+      require(verify(
+          owner,
+          player, 0, tokenURI, _nonce,
+          signature
+      ), "ERC721: must be validated by Gitcoin");
+      require(balanceOf(player) == 0, "Address cannot have more than 1 passport");
       uint256 newItemId = totalSupply();
+      require(_nonce > newItemId, "Invalid Nonce");
+
       _mint(player, newItemId);
       _setTokenURI(newItemId, tokenURI);
       return newItemId;
   }
+
+    function noxfr() internal {
+        require(false, "GitcoinPassport's cannot be transfered, try burning this token + minting a new one.");
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+      noxfr();
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override {
+      noxfr();
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public override {
+      noxfr();
+    }
+
+    function burn(uint256 tokenId) public {
+      require(ERC721.ownerOf(tokenId) == msg.sender, "ERC721: transfer of token that is not own");
+      _burn(tokenId);
+    }
 
 }
--- test/GitcoinPassport.js
+++ test/GitcoinPassport.js
@@ -22,36 +22,71 @@ describe("GitcoinPassport", function() {
   });
 
   it("Test Minting", async function() {
+    // setup
     const GitcoinPassport = await ethers.getContractFactory("GitcoinPassport");
     const sp = await GitcoinPassport.deploy();
     const accounts = await ethers.getSigners();
-    
+
+    // validate that a first passport can be created
     await sp.deployed();
     const player = accounts[1].address
     const tokenURI = "/foo/bar/now";
     const idx = 0;
-
-    // first token
-    await sp.createPassport(player, tokenURI);
+    const nonce = 5;
+    const hash = await sp.getMessageHash(player, 0, tokenURI, nonce);
+    const signature = await accounts[0].signMessage(ethers.utils.arrayify(hash));
+    await sp.connect(accounts[1]).createPassport(tokenURI, signature, nonce);
     expect(await sp.totalSupply()).to.equal(1);
     expect(await sp.tokenURI(idx)).to.equal(tokenURI);
     var foo = await sp.tokenByIndex(idx)
 
-    // second token
+    // validate that a second passport can be created, and txn can be sent by a diff account
     const idx_new = 1;
     const player_new = accounts[2].address
     const tokenURI_new = "/foo/bar/now";
-    await sp.createPassport(player_new, tokenURI_new);
+    const hash_new = await sp.getMessageHash(player_new, 0, tokenURI_new, nonce);
+    const signature_new = await accounts[0].signMessage(ethers.utils.arrayify(hash_new));
+    await sp.connect(accounts[2]).createPassport(tokenURI_new, signature_new, nonce);
     expect(await sp.totalSupply()).to.equal(2);
     expect(await sp.tokenURI(idx_new)).to.equal(tokenURI_new);
-
+    
     // test that only Gitcoin can mint these
     try{
-        await sp.connect(accounts[2]).createPassport(player_new, tokenURI_new);
+        const signature_newest = await accounts[1].signMessage(ethers.utils.arrayify(hash_new));
+        await sp.connect(accounts[2]).createPassport(tokenURI_new, signature_newest, nonce);
         expect(false).to.equal(true);
     } catch(e){
         expect(true).to.equal(true);
     }
+
+    // validate passport can't be created twice with same player
+    try{
+        await sp.connect(accounts[2]).createPassport(tokenURI_new, signature_new, nonce);
+        expect(false).to.equal(true);
+    } catch(e){
+        expect(true).to.equal(true);
+    }
+
+    // validate passport can't be transfered
+    try{
+        await sp.connect(accounts[0]).transferFrom(accounts[0].address, accounts[1].address, 0);
+        expect(false).to.equal(true);
+    } catch(e){
+        expect(true).to.equal(true);
+    }
+
+    // validate passport can't be burned except by its owner
+    try{
+        await sp.connect(accounts[0]).burn(1);
+        expect(false).to.equal(true);
+    } catch(e){
+        expect(true).to.equal(true);
+    }
+
+    // validate passport CAN be burned by its owner
+    expect(await sp.balanceOf(accounts[1].address)).to.equal(1);
+    await sp.connect(accounts[1]).burn(0);
+    expect(await sp.balanceOf(accounts[1].address)).to.equal(0);
 
 
   });
